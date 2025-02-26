d5dc4575d5608beee50c4640d8200b47480ad78b
feryafox <bogdashkin145@gmail.com>
2025-02-26 20:12:46 +0300

    WIP

--- ProductService/src/main/java/ru/feryafox/productservice/services/kafka/KafkaProducerService.java
+++ ProductService/src/main/java/ru/feryafox/productservice/services/kafka/KafkaProducerService.java
@@ -1,7 +1,16 @@
 package ru.feryafox.productservice.services.kafka;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import ru.feryafox.kafka.models.ProductEvent;
 
 @Service
+@RequiredArgsConstructor
 public class KafkaProducerService {
+    private final KafkaTemplate<String, ProductEvent> kafkaTemplate;
+
+    public void sendProductEvent(ProductEvent productEvent) {
+        kafkaTemplate.send("product-events", productEvent.getProductId(), productEvent);
+    }
 }
---
+++ shared-library/src/main/java/ru/feryafox/kafka/models/ProductEvent.java
@@ -0,0 +1,23 @@
+package ru.feryafox.kafka.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductEvent implements BaseKafkaModel{
+    private final String type = "ProductEvent";
+
+    private String productId;
+    private String shopId;
+    private String ownerId;
+    private String name;
+
+    public enum ShopStatus {
+        CREATED,
+        UPDATED,
+        DELETED
+    }
+}
