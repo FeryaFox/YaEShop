bfeb8389448e517df910dd1f77bd3743d27f7eef
FeryaFox <bogdashkin145@gmail.com>
2025-03-19 16:26:07 +0300

    Небольшие исправления

--- ProductService/src/main/java/ru/feryafox/productservice/repositories/elastic/ProductSearchRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.feryafox.productservice.repositories.elastic;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import ru.feryafox.productservice.entities.elastic.ProductIndex;
-
-import java.util.List;
-
-public interface ProductSearchRepository extends ElasticsearchRepository<ProductIndex, String> {
-    List<ProductIndex> findByNameContaining(String name);
-}
